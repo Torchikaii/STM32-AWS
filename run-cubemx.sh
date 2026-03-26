d8778f1fc08c7212fa5df33b5f44dad0ba00092b
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-03-26 16:46:46 +0200

    add auto install script
    paths may be adjusted as in /opt folder cubemx stopped finding .ioc file

--- run-cubemx.sh
+++ run-cubemx.sh
@@ -3,11 +3,11 @@ set -euo pipefail
 
 SCRIPT_PATH="${1:-generate_script.txt}"
 SCRIPT_PATH="$(realpath "$SCRIPT_PATH")"
-CUBEMX_PATH="${CUBEMX_PATH:-$HOME/STM32CubeMX/STM32CubeMX}"
+CUBEMX_PATH="${CUBEMX_PATH:-/opt/STM32CubeMX/STM32CubeMX}"
 
 echo "Using SCRIPT_PATH=$SCRIPT_PATH"
 echo "Using CUBEMX_PATH=$CUBEMX_PATH"
 echo "Using HOME=$HOME"
 
-xvfb-run -a -s "-screen 0 1024x768x24" \
-    "$CUBEMX_PATH" -q "$SCRIPT_PATH"
+
+"$CUBEMX_PATH" -q "$SCRIPT_PATH"
--- setup-repo.sh
+++ setup-repo.sh
@@ -2,24 +2,32 @@
 
 set -e
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+CUBEMX_AUTO_XML="$SCRIPT_DIR/cubemx-auto.xml"
+CUBEMX_INSTALL_DIR="/opt/STM32CubeMX"
+
 echo "setup-repo.sh running"
 
 sudo apt update -y
-sudo apt install 7z -y
-sudo apt install make -y
-sudo apt install build-essential -y
-sudp apt install libusb-1.0-0-dev -y
-sudo apt install gcc-arm-none-eabi -y
-sudo apt install gdb-multiarch -y
-sudo apt install openocd -y
+sudo apt install -y \
+    p7zip-full \
+    make \
+    build-essential \
+    libusb-1.0-0-dev \
+    gcc-arm-none-eabi \
+    gdb-multiarch \
+    openocd 
+    
 
-
+cd /tmp
 
 wget https://stm32-cube-mx.s3.eu-central-1.amazonaws.com/stm32cubemx-lin-v6-15-0.zip -O stm32cubemx.zip
-7z x stm32cubemx.zip
+7z x -y stm32cubemx.zip
 chmod +x SetupSTM32CubeMX-*
-./SetupSTM32CubeMX-6.15.0 -c --option-file /path/to/cubemx-auto.xml
+sudo ./SetupSTM32CubeMX-6.15.0 -c --option-file "$CUBEMX_AUTO_XML"
+rm -f stm32cubemx.zip SetupSTM32CubeMX-*
 
-
+echo ""
+echo "CubeMX installed to: $CUBEMX_INSTALL_DIR"
 echo "setup-repo.sh completed"
 
