41566a18141c6d460d81c804c7167c6bd8650cb2
Torchikaii <72319744+Torchikaii@users.noreply.github.com>
2026-03-30 22:27:10 +0300

    update scripts

--- generate_script.sh
+++ generate_script.sh
@@ -1,14 +1,16 @@
+#!/usr/bin/env bash
+
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 IOC_PATH="$SCRIPT_DIR/test439/test439.ioc"
 PROJECT_PATH="$SCRIPT_DIR/test439"
 
-echo "login \"${ST_CUBE_EMAIL}\" \"${ST_CUBE_PASSWORD}\" y" > generate_script.txt
-echo "swmgr refresh" >> generate_script.txt
-echo "swmgr install stm32cube_f4_1.28.2 ask" >> generate_script.txt
-echo "config load \"$IOC_PATH\"" >> generate_script.txt
-echo "project toolchain \"Makefile\"" >> generate_script.txt
-echo "project path \"$PROJECT_PATH\"" >> generate_script.txt
-echo "project name \"test439\"" >> generate_script.txt
-echo "project generateunderroot 1" >> generate_script.txt
-echo "project generate" >> generate_script.txt
-echo "exit" >> generate_script.txt
+echo "login \"${ST_CUBE_EMAIL}\" \"${ST_CUBE_PASSWORD}\" y" > "$SCRIPT_DIR/generate_script.txt"
+echo "swmgr refresh" >> "$SCRIPT_DIR/generate_script.txt"
+echo "swmgr install stm32cube_f4_1.28.3 ask" >> "$SCRIPT_DIR/generate_script.txt"
+echo "config load \"$IOC_PATH\"" >> "$SCRIPT_DIR/generate_script.txt"
+echo "project toolchain \"Makefile\"" >> "$SCRIPT_DIR/generate_script.txt"
+echo "project path \"$PROJECT_PATH\"" >> "$SCRIPT_DIR/generate_script.txt"
+echo "project name \"test439\"" >> "$SCRIPT_DIR/generate_script.txt"
+echo "project generateunderroot 1" >> "$SCRIPT_DIR/generate_script.txt"
+echo "project generate" >> "$SCRIPT_DIR/generate_script.txt"
+echo "exit" >> "$SCRIPT_DIR/generate_script.txt"
